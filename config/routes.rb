ed6cdd6dfa5f150fbdf912d5cec91352efbfb2a0
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-23 19:35:28 +0000

    at long last the counter shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "counter", to: "counter#show"
   resources :posts
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
