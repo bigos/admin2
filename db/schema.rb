2c1a6741bfd0115d4fc40022d523b8e7d3e09c56
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-23 16:24:22 +0000

    added scaffold for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_23_160330) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_23_162328) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
