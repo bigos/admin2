3acb8ade7f6cd993fdd2e3e084813d6abf39cb35
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-24 21:51:43 +0000

    preparing to list users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  devise_for :users
+
   get "counter", to: "counter#show"
   resources :posts
-  devise_for :users
+  resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
