50f039e8be88090e8cdf4860816659b9cb0b7c8c
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-24 22:23:11 +0000

    annotated models

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem 'annotate'
   gem "html2haml" # only needed for erb to haml migration
   gem "web-console"
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  post_id    :integer
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
 end
