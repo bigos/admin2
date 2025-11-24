4599990e6cbbc7b78245eaf0d5fc5c01a8283644
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-24 22:07:56 +0000

    users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show ]
+
+  respond_to :html
+
   def index
+    @users = User.all
+    respond_with(@users)
   end
 
   def show
+    respond_with(@user)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:title, :body)
   end
 end
