d33f57c6cfaecbe8e37af5eb9d2276c67176b34e
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-24 21:28:32 +0000

    seeding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.seed_users
+    usnx = "matthew mark luke john".split(' ')
+    usnx.each do |un|
+      uemail = "#{un}@example.com"
+      next if User.where(email: uemail).present?
+
+      u = User.new
+      u.email = uemail
+      u.password = "secret"
+      u.save
+    end
+  end
 end
